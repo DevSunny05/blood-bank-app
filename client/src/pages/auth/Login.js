06290276269f316cbb1ff06573f4fabf968449f2
sarang <sarang.office01@gmail.com>
2023-09-04 12:26:50 +0530

    reusable input for components created

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import InputType from '../../components/shared/forms/InputType'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+        <div className="row">
+            <div className="col-md-8 form-banner">
+                <img src="./assets/banner1.jpg" alt="banner1" />
+            </div>
+            <div className="col-md-4 form-container">
+                <form>
+                   <InputType label={'Email'} labelFor={'forEmail'} name={'email'} type={'email'} />
+                   <InputType label={'Password'} labelFor={'forPassword'} name={'password'} type={'password'} />
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+
+             </div>
+        </div>
+    </>
   )
 }
 
