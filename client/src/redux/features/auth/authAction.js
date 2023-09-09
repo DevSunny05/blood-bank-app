ed854f9492d66b2f74440f54af94773068db97e0
sarang <sarang.office01@gmail.com>
2023-09-09 15:52:41 +0530

    private and public routes created

--- client/src/redux/features/auth/authAction.js
+++ client/src/redux/features/auth/authAction.js
@@ -4,17 +4,22 @@ import {  toast } from 'react-toastify';
 
 
 
+
+
 export const userLogin=createAsyncThunk(
     'auth/login',
     async({role,email,password},{rejectWithValue})=>{
         try {
             const {data}=await API.post('/auth/login',{role,email,password})
             // store token
+            
             if(data.success){
                 localStorage.setItem('token',data.token)
-                toast.success(data.message)
+                toast.success(data.message)  
             }
-            return data;
+
+            return data
+            
         } catch (error) {
             if(error.response && error.response.data.message){
                 return rejectWithValue(error.response.data.message)
@@ -33,15 +38,33 @@ export const userRegister=createAsyncThunk(
     async ({name ,role,email,password,organisationName,hospitalName,website,address,phone},{rejectWithValue})=>{
         try {
             const {data}=await API.post('/auth/register',{name ,role,email,password,organisationName,hospitalName,website,address,phone})
-          
+           
             if(data.success){
                 toast.success(data.message)
-                
+               
             }
             
             
             return data
 
+        } catch (error) {
+            if(error.response && error.response.data.message){
+                return rejectWithValue(error.response.data.message)
+            }else{
+                return rejectWithValue(error.message)
+            }
+        }
+    }
+)
+
+export const getCurrentUser=createAsyncThunk(
+    'auth/getCurrentUser',
+    async ({rejectWithValue})=>{
+        try {
+            const res=await API.get('/auth/current-user')
+            if(res?.data){
+                return res?.data
+            }
         } catch (error) {
             if(error.response && error.response.data.message){
                 return rejectWithValue(error.response.data.message)
--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { userLogin, userRegister } from "./authAction";
+import { getCurrentUser, userLogin, userRegister } from "./authAction";
 
 const token=localStorage.getItem('token')?localStorage.getItem('token'):null;
 
@@ -25,7 +25,7 @@ const authSlice=createSlice({
             state.loading=false;
             state.user=payload.user;
             state.token=payload.token
-            
+
         })
         builder.addCase(userLogin.rejected,(state,{payload})=>{
             state.loading=false;
@@ -45,6 +45,24 @@ const authSlice=createSlice({
             
         })
         builder.addCase(userRegister.rejected,(state,{payload})=>{
+            state.loading=false;
+            state.error=payload
+
+        })
+
+        
+        // get current user
+        builder.addCase(getCurrentUser.pending,(state)=>{
+            state.loading=true;
+            state.error=null
+        })
+
+        builder.addCase(getCurrentUser.fulfilled,(state,{payload})=>{
+            state.loading=false;
+            state.user=payload.user
+            
+        })
+        builder.addCase(getCurrentUser.rejected,(state,{payload})=>{
             state.loading=false;
             state.error=payload
 
