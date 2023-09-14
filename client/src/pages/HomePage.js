db7e67f441f49608d92b0580c9026b8374992260
sarang <sarang.office01@gmail.com>
2023-09-14 11:42:17 +0530

    work on layout and logout functionality

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import Spinner from '../components/shared/Spinner'
+import Layout from '../components/layout/Layout'
 
 const HomePage = () => {
+    const {loading}=useSelector(state=>state.alerts)
   return (
-    <div>HomePage</div>
+    <Layout>
+        {
+            loading ?<Spinner/>:(
+                <>
+                <h1>Home Page</h1>
+                </>
+            )
+        }
+    </Layout>
   )
 }
 
